ab269163525fa70d1f03dcee43a607daeadb50c9
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-06-18 19:21:20 -0400

    feat: agregar morgan como middleware para el registro de solicitudes

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import morgan from 'morgan'
 
 import subscriptionRouter from '@/routes/subscriptionRoutes.js'
-import userRouter from '@/routes/userRoutes.js'
+// import userRouter from '@/routes/userRoutes.js'
 import authRouter from '@/routes/authRoutes.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(morgan('dev'))
 
 app.use('/api/subs', subscriptionRouter)
 // app.use("/api/users", userRouter)
