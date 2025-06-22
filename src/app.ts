ce2987e824113b18c318e3573ca70a48124130ca
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-06-21 22:10:02 -0400

    fix: simplificar la configuración de CORS en la aplicación

--- src/app.ts
+++ src/app.ts
@@ -12,10 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan('dev'))
-app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:5173',
-  credentials: true,
-}))
+app.use(cors())
 
 app.use('/api/subs', subscriptionRouter)
 // app.use("/api/users", userRouter)
