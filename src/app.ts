3db8b63fa7592748211c10f0afaa7f7c86d1e4e3
Omar Bencosme Tio <obencosmetio@gmail.com>
2025-06-27 18:59:20 -0400

    fix: cambiar el formato de registro de morgan de 'dev' a 'common'

--- src/app.ts
+++ src/app.ts
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(morgan('dev'))
+app.use(morgan('common'))
 app.use(cors())
 
 app.use('/api/subs', subscriptionRouter)
